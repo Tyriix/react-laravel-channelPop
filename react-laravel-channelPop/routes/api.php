928f8b76a0ff09c54a30f2996f421c6914ef8dda
Tyriix <konrad.barszcz1@gmail.com>
2023-07-17 23:01:11 +0200

    Moved root

--- react-laravel-channelPop/routes/api.php
+++ react-laravel-channelPop/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\http\Controllers\ChannelPopController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::resource('channelpops', ChannelPopController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
