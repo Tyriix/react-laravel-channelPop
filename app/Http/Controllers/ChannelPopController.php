1a7d84bf7eed019552e5cec506981426b14caa8d
Tyriix <konrad.barszcz1@gmail.com>
2023-07-17 21:29:16 +0200

    Initial commit

---
+++ app/Http/Controllers/ChannelPopController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChannelPop;
+use Illuminate\Http\Request;
+
+class ChannelPopController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ChannelPop $channelPop)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ChannelPop $channelPop)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ChannelPop $channelPop)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ChannelPop $channelPop)
+    {
+        //
+    }
+}
