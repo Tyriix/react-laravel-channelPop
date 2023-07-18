e40f512e1cc378b5361d9bd805c94573d914d04d
Tyriix <konrad.barszcz1@gmail.com>
2023-07-18 16:16:32 +0200

    Added creating channels

--- channel-pop-react/src/App.tsx
+++ channel-pop-react/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router , Routes, Route, Link } from "react-router-dom";
+import {Navbar, Container, Row, Col} from "react-bootstrap";
+import CreateChannelPop from './components/channelPop/create.component';
+import List from './components/channelPop/list.component';
+import UpdateChannel from './components/channelPop/update.component';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<Router>
+    <Navbar bg="primary">
+      <Container>
+        <Link to={"/"} className="navbar-brand text-white">
+          Basic Crud App
+        </Link>
+      </Container>
+    </Navbar>
+
+    <Container className="mt-5">
+      <Row>
+        <Col md={12}>
+          <Routes>
+            <Route path="/channel/create" element={<CreateChannelPop />} />
+            <Route path='/channel/update/:id' element={<UpdateChannel/>} />
+            <Route path='/' element={<List />}/>
+          </Routes>
+        </Col>
+      </Row>
+    </Container>
+  </Router>);
 }
 
 export default App;
---
+++ channel-pop-react/src/components/channelPop/create.component.tsx
@@ -0,0 +1,100 @@
+import React, {useState} from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import axios from 'axios'
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom'
+
+export default function CreateChannelPop(){
+    const navigate = useNavigate();
+
+    const [channelName, setChannelName] = useState("");
+    const [population, setPopulation] = useState("");
+    const [validationError,setValidationError] = useState({})
+
+    const CreateChannelPop = async (e: any) => {
+        e.preventDefault();
+        const data = new FormData();
+
+        data.append('channelName', channelName);
+        data.append('population', population);
+
+        await axios.post(`http://localhost:8000/api/channels`, data).then(({data})=>{
+        Swal.fire({
+            icon:"success",
+            text:data.message
+        })
+        navigate("/")
+    }).catch(({response})=>{
+        if(response.status===442)
+        setValidationError(response.data.errors)
+        else{
+            Swal.fire({
+                icon:"error",
+                text:response.data.message
+            })
+        }
+    })
+    }
+    return(
+        <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-12 col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">Create Channel</h4>
+                <hr />
+                <div className="form-wrapper">
+                  {
+                    Object.keys(validationError).length > 0 && (
+                      <div className="row">
+                        <div className="col-12">
+                          <div className="alert alert-danger">
+                            <ul className="mb-0">
+                              {
+                                Object.entries(validationError).map(([key, value]: any)=>(
+                                  <li key={key}>{value}</li>   
+                                ))
+                              }
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  }
+                  <Form onSubmit={CreateChannelPop}>
+                    <Row> 
+                        <Col>
+                          <Form.Group controlId="ChannelName">
+                              <Form.Label>Channel Name</Form.Label>
+                              <Form.Control type="text" value={channelName} onChange={(event)=>{
+                                setChannelName(event.target.value)
+                              }}/>
+                          </Form.Group>
+                        </Col>  
+                    </Row>
+                    <Row className="my-3">
+                        <Col>
+                          <Form.Group controlId="Population">
+                              <Form.Label>Population</Form.Label>
+                              <Form.Control as="textarea" rows={3} value={population} onChange={(event)=>{
+                                setPopulation(event.target.value)
+                              }}/>
+                          </Form.Group>
+                        </Col>
+                    </Row>
+                    <Button variant="primary" className="mt-2" size="lg" type="submit">
+                      Save
+                    </Button>
+                  </Form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
