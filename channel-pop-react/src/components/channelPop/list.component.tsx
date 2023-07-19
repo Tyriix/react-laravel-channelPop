ab61c74bf59a81c6e7e2a06bbc7aed4dc1f4490b
Tyriix <konrad.barszcz1@gmail.com>
2023-07-19 12:28:31 +0200

    Started tests

--- channel-pop-react/src/components/channelPop/list.component.tsx
+++ channel-pop-react/src/components/channelPop/list.component.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import PieChart from "../pieChart.component";
+import PieChart from "./pieChart.component";
 import { Table, Col, Row, Button } from "react-bootstrap";
 import Swal from "sweetalert2";
 export default function List() {
